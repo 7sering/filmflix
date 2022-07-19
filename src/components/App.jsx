4f67a96e1e58e075b26df438ed170b2c38835ed7
7sering <codenow8@gmail.com>
2022-07-19 12:52:59 +0545

    file and folder structure complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+// import { ClassNames } from '@emotion/react';
+import { MovieInformation, Movies, Navbar, Actors, Profile } from './index';
+import useStyles from './style';
 
-const App = () => (
-  <div>
-    <center>  <h2>FilmFlix Project</h2></center>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+  console.log(classes);
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+        </Routes>
+      </main>
+
+    </div>
+  );
+};
 
 export default App;
+
